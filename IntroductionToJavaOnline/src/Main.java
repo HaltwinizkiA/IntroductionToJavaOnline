f2377179b01e50e4d638ff2be68e960f0a89cebc
crptbosyak <goldsashko@gmail.com>
2021-03-07 13:42:48 +0300

    ad

--- IntroductionToJavaOnline/src/Main.java
+++ IntroductionToJavaOnline/src/Main.java
@@ -1,9 +1,10 @@
-
+import Programming_with_classes.bank.Bank;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Bank bank=new Bank();
+        bank.createBank();
 
 
 
---
+++ IntroductionToJavaOnline/src/Programming_with_classes/bank/Account.java
@@ -0,0 +1,22 @@
+package Programming_with_classes.bank;
+
+public class Account {
+    int id;
+    double value;
+    boolean state;
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", value=" + value +
+                ", state=" + state +
+                '}';
+    }
+
+    public Account(int id, double value, boolean state) {
+        this.id = id;
+        this.value = value;
+        this.state = state;
+    }
+}
