773281ad15b132aaab1b738801738f997c2991eb
crptbosyak <goldsashko@gmail.com>
2021-03-11 15:20:18 +0300

    ad

---
+++ IntroductionToJavaOnline/src/Basics_of_OOP/text_file/File.java
@@ -0,0 +1,5 @@
+package Basics_of_OOP.text_file;
+
+public class File {
+    String text="";
+}
---
+++ IntroductionToJavaOnline/src/Basics_of_OOP/text_file/TextFile.java
@@ -0,0 +1,50 @@
+package Basics_of_OOP.text_file;
+
+import java.util.Scanner;
+
+public class TextFile {
+    Directory directory;
+    File file;
+    Scanner scanner = new Scanner(System.in);
+
+    public TextFile createText() {
+        TextFile textFile = new TextFile();
+        textFile.file = new File();
+        textFile.directory = new Directory();
+        int i = 0;
+        while (!(i == 4)) {
+            i = scanner.nextInt();
+            switch (i) {
+                case 1 -> supplement(textFile);
+                case 2 -> outPut(textFile);
+                case 3 -> remove(textFile);
+
+
+            }
+        }
+return textFile;
+    }
+
+    public void outPut(TextFile textFile) {
+
+        System.out.println("way:" + textFile.directory.way + " Content :" + textFile.file.text);
+    }
+
+    public TextFile remove(TextFile textFile) {
+
+        return null;
+    }
+
+    public TextFile supplement(TextFile textFile) {
+        System.out.println("enter supplements:");
+
+        String app = scanner.next();
+        textFile.file.text = textFile.file.text + " " + app;
+        return textFile;
+
+    }
+}
+
+
+//    Создать объект класса Текстовый файл, используя классы Файл, Директория. Методы: создать, переименовать,
+//        вывести на консоль содержимое, дополнить, удалить.
