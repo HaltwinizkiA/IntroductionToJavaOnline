4b05e62f91f1f277c4cee0877ca5263635d50302
crptbosyak <goldsashko@gmail.com>
2021-03-11 11:23:07 +0300

    ad

--- IntroductionToJavaOnline/src/Main.java
+++ IntroductionToJavaOnline/src/Main.java
@@ -1,16 +1,6 @@
-
-
 public class Main {
 
     public static void main(String[] args) {
-        a(12,123,123);
-        a(12);
 
-    }
-    public static void a(int...d){
-        System.out.println(1);
-    }
-    public static void a(int d){
-        System.out.println(2);
     }
 }
