058a56c96f8b4d42912506a469fc89c476936b54
crptbosyak <goldsashko@gmail.com>
2021-03-18 19:08:41 +0300

    ad

--- IntroductionToJavaOnline/src/tasks/task3/Archive.java
+++ IntroductionToJavaOnline/src/tasks/task3/Archive.java
@@ -1,6 +1,65 @@
 package tasks.task3;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Archive {
+    public Archive() {
+        try {
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Socket clientSocket; //сокет для общения
+    private static ServerSocket server; // серверсокет
+    private static BufferedReader in; // поток чтения из сокета
+    private static BufferedWriter out; // поток записи в сокет
+
+    public static void main(String[] args) {
+        try {
+            try  {
+                server = new ServerSocket(4004); // серверсокет прослушивает порт 4004
+                System.out.println("Сервер запущен!"); // хорошо бы серверу
+                //   объявить о своем запуске
+                clientSocket = server.accept(); // accept() будет ждать пока
+                //кто-нибудь не захочет подключиться
+                try { // установив связь и воссоздав сокет для общения с клиентом можно перейти
+                    // к созданию потоков ввода/вывода.
+                    // теперь мы можем принимать сообщения
+                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    // и отправлять
+                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                    String word = in.readLine(); // ждём пока клиент что-нибудь нам напишет
+                    System.out.println(word);
+                    // не долго думая отвечает клиенту
+                    out.write("Привет, это Сервер! Подтверждаю, вы написали : " + word + "\n");
+                    out.flush(); // выталкиваем все из буфера
+
+                } finally { // в любом случае сокет будет закрыт
+                    clientSocket.close();
+                    // потоки тоже хорошо бы закрыть
+                    in.close();
+                    out.close();
+                }
+            } finally {
+                System.out.println("Сервер закрыт!");
+                server.close();
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+
+
 
 
 
