37c317a378530542d4564b5ee6c0c2f30187279e
crptbosyak <goldsashko@gmail.com>
2021-03-30 21:17:31 +0300

    ad

--- IntroductionToJavaOnline/src/Main.java
+++ IntroductionToJavaOnline/src/Main.java
@@ -1,19 +1,16 @@
-import tasks.task4.container.Containter;
-import tasks.task4.container.SortConatiner;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Containter> containterList=new ArrayList<Containter>();
-        containterList.add(new Containter(10));
-        containterList.add(new Containter(12));
-        containterList.add(new Containter(1));
-        containterList.sort(new SortConatiner());
+
+
+
 
 
     }
 }
+
+
+
