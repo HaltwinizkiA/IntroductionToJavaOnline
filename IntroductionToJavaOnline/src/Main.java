42b8eaa0fa56d2a32943ae40cddeb8a2b4113bbd
crptbosyak <goldsashko@gmail.com>
2021-04-02 16:00:49 +0300

    ad

--- IntroductionToJavaOnline/src/Main.java
+++ IntroductionToJavaOnline/src/Main.java
@@ -1,16 +1,24 @@
-
-
 public class Main {
 
     public static void main(String[] args) {
 
 
+        try{
+            String[] a=new String[1];
+            try {
+                a[2]="asdf";
 
+            } catch (ArithmeticException s){
 
+            } catch (IndexOutOfBoundsException|ArithmeticException e){
+                System.out.println(2);
+                int b=123/0;
+            }
 
-
-    }
 }
+}
+
+
 
 
 
